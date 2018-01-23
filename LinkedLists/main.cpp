16f3d1ec5fbeda102d6f668eb166e26ec8f3b5f0
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-01-23 10:25:41 -0600

    fixed various functions

--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -4,6 +4,8 @@ int main()
 	NodeType<int> a = { 12 };
 	LinkedListType<int> *test = new LinkedListType<int>;
 	LinkedListType<int> *test2 = new LinkedListType<int>;
+	LinkedListType<int> *Last = new LinkedListType<int>;
+	Last->InsertLast(3);
 	test->InitializeList();
 	test->InsertFirst(12);
 	test->InsertFirst(8);
@@ -11,8 +13,13 @@ int main()
 	test->InsertLast(40);
 	test2->InsertFirst(1);
 	test2->InsertLast(22);
+	test->DeleteNode(12);
+	assert(test->Search(8) == true);
+	assert(test->Search(12) == false);
 	test = test2;
 	test->Print();
 	test2->Print();
+	test->Begin();
+	test->End();
 	LinkedListType<int>test3(*test);
 }
